d16cc97179963f0ea1418f3c76c7104bf11033a2
twendlandt <twendlan@ualberta.ca>
2014-11-04 02:22:14 -0700

    adding very-draft sequential-halving alg

--- Algorithms/lil_UCB.py
+++ Algorithms/lil_UCB.py
@@ -4,11 +4,11 @@ from numpy import *
 def lil_UCB(arms,delta=1,epsilon=1,lambda=1,beta=1,sigma=1):
 """
 Parameters:
-* arms is a row vector containing ID of different arms. 
+* arms is a row vector containing ID of different arms.
 * Other parameters are just different constants
 * T is a row vector having the number of times an arm is pulled after n iterations. So T[i] represents the same arm that arms[i] represents
 * mu[i] represents mean value of arm i
-* upper_bound is a vector that has upper bounds of every arms in a single iteration. After calculating upper_bound, the algorithm chooses that arm that has the maximum upper bound from upper_bound vector. 
+* upper_bound is a vector that has upper bounds of every arms in a single iteration. After calculating upper_bound, the algorithm chooses that arm that has the maximum upper bound from upper_bound vector.
 
 """
 	timestep=0
@@ -22,7 +22,7 @@ Parameters:
 		mu[i]=r
 
 
-	while True:	
+	while True:
 
 		counter=0
 
@@ -38,7 +38,7 @@ Parameters:
 		upper_bound=zeros(n)
 		# for j in range(0,n):
 
-			# upper_bound[j]=mu[j] + (1+beta)*(1+sqrt(epsilon))*sqrt((2*(sigma**2)*(1+epsilon)*math.log((math.log((1+epsilon)*T[j]))/delta))/T[j])
+		# upper_bound[j]=mu[j] + (1+beta)*(1+sqrt(epsilon))*sqrt((2*(sigma**2)*(1+epsilon)*math.log((math.log((1+epsilon)*T[j]))/delta))/T[j])
 		upper_bound=mu + (1+beta)*(1+sqrt(epsilon))*sqrt((2*(sigma**2)*(1+epsilon)*math.log((math.log((1+epsilon)*T))/delta))/T)
 
 		index=upper_bound.argmax()
--- Algorithms/sequential_halving.py
+++ Algorithms/sequential_halving.py
@@ -1,19 +1,53 @@
 import math
 import numpy
 
+class sampleArm():
+
+    # as of right now I am assuming an arm is represented by a numpy array
+    def __init__(self, arm):
+        self.arm = arm
+        self.total_reward = 0
+        self.average = 0
+
+    def get_arm():
+        return arm
+
+    def set_arm(self, arm):
+        self.arm = arm
+
+    def get_total_reward():
+        return total_reward
+
+    def set_total_reward(self, total_reward):
+        self.total_reward = total_reward
+
+    def get_average():
+        return average
+
+    def set_average(self, average):
+        self.average = average
+
+
 def sequential_halving(arms, bound):
     """
     Sequential Halving -- (Fixed Bound)
     Parameters:
-    * let arms be a an array where each entry is an arm and all of
-        it's simulated results
-    * bound is an integer which represents the maximum number of 
+    * let arms be an array of vectors, where each vector is a configuration
+    * bound is an integer which represents the maximum number of
         arms pulls the algorithm is limited to find optimal arms with
     ** still unsure how we will set up our s, right now it is a list of
         the array arms, which shrinks by half on each iteration
     """
     s = list()
-    s.append(arms)
+
+    # add arms with initialized values to the list
+    for i in range(len(arms)):
+        a = list()
+        arm = new sampleArm(arms[i])
+        a.append(arm)
+    s.append(a)
+
+    # run through iterations of algorithm
     for r in range(math.log2(len(arms)) - 1):
 
         # compute the pulls per arm per iteration
@@ -28,13 +62,11 @@ def sequential_halving(arms, bound):
                 # sample arm i from remaining arms pulls_per_arm times
                 total += sample(s[r][i])
             s[r][i].setAverage(total/pulls_per_arm)
-        
+
         # sort the remaining arms by average from above sample
-        s[r] = sortArms(s[r])
+        s[r] = s[r].sort(key=lambda x: x.get_average, reverse=False)
 
         # create next iteration which the upper half of the sorted list
         s.append(math.ceil(s[r][(len(s[r])/2):]))
 
     return s[math.log2(len(arms))]
-            
-        
