cccefbba4a6a0c6cf196aa686ba7e89c5c7b9368
twendlandt <twendlan@ualberta.ca>
2014-11-23 17:01:01 -0700

    edited gitignore, added data/arms/summary files

--- Simulator/Logs/log_parser.py
+++ Simulator/Logs/log_parser.py
@@ -4,9 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
-def parse_deltas(log_file, delta_list):
-    delta = 0.0
-    pull = 0
+def parse_deltas(log_file):
+    delta_list = list()
+    pull_list = list()
+    pull_dict = dict()
+    pull_count = dict()
 
     for line in log_file:
         info = line.split()
@@ -17,10 +19,29 @@ def parse_deltas(log_file, delta_list):
                     delta = float(info[i+1].strip('<>:,'))
                 elif var == "PULLS":
                     pull = int(info[i+1].strip('<>:,'))
-            delta_list.append((delta, pull))        
+                    if pull_dict.has_key(pull):
+                        pull_dict[pull] += delta
+                        pull_count[pull] += 1
+                    else:
+                        pull_dict[pull] = delta
+                        pull_count[pull] = 1
+                        pull_list.append(pull)
+    
+    # iterate through and average deltas
+    for pull in pull_list:
+        delta = pull_dict[pull] / pull_count[pull]
+        delta_list.append((delta, pull))        
     log_file.seek(0, 0)
+    return delta_list
 
-def parse_best_arm_deltas(log_file, best_delta_list):
+def write_deltas_to_file(delta_file, timestep_file, delta_list):
+    for delta in delta_list:
+        delta_file.write(str(delta[0]) + "\n")
+        timestep_file.write(str(delta[1]) + "\n")
+
+def parse_best_arm_deltas(log_file):
+    best_delta_list = list()
+
     for line in log_file:
         info = line.split()
         if info and info[0] == 'BEST':
@@ -29,12 +50,13 @@ def parse_best_arm_deltas(log_file, best_delta_list):
                 if var == "DELTA":
                     best_delta_list.append(float(info[i+1].strip('<>:,')))
     log_file.seek(0, 0)
+    return best_delta_list
 
 def parse_arm_distribution(log_file):
-    arm_dict = dict()
-    arm_count = dict()
-    arm_list = list()
-    arm_deltas = list()
+    arm_dict = dict()           # holds average configmu for an arm
+    arm_count = dict()          # holds how many times an arm has occurred
+    arm_list = list()           # holds list of all arms, used for iteration
+    arm_configmus = list()      # the list of average configmus to be returned 
     
     for line in log_file:
         info = line.split()
@@ -45,18 +67,22 @@ def parse_arm_distribution(log_file):
             for i in range(len(info)):
                 var = info[i].strip('<>:,')
                 if var == "CONFIGMU":
-                    delta = float(info[i+1].strip('<>:,'))
+                    configmu = float(info[i+1].strip('<>:,'))
                     if arm in arm_dict:
                         arm_count[arm] = arm_count[arm] + 1
-                        arm_dict[arm] = (arm_dict[arm] + delta) / arm_count[arm]
+                        arm_dict[arm] += configmu 
                     else:
                         arm_list.append(arm)
                         arm_count[arm] = 1
-                        arm_dict[arm] = delta
+                        arm_dict[arm] = configmu 
     for arm in arm_list:
-        arm_deltas.append(arm_dict[arm])
-    log_file.seek(0, 0)
-    return arm_deltas
+        arm_configmus.append(arm_dict[arm] / arm_count[arm])
+    log_file.seek(0, 0)         # point to the start of the logfile
+    return arm_configmus
+
+def write_arms_to_file(arm_file, arm_configmus):
+    for arm in arm_configmus:
+        arm_file.write(str(arm) + '\n')
 
 def average_delta(d_list):
     return sum(d_list)/len(d_list)
@@ -67,6 +93,14 @@ def success_rate(delta_list, threshold):
         if delta < threshold:
             success += 1.0
     return success / len(delta_list)
+
+def write_summary_stats_to_file(summary_file, best_delta_list):
+    # summary statistics
+    threshold = 0.01
+    summary_file.write("Average Best Arm Delta: %f\n" 
+            %(average_delta(best_delta_list)))
+    summary_file.write("Success Rate with threshold: %f, is: %f\n" 
+            %(threshold, success_rate(best_delta_list, threshold)))
 
 def plot_egreedy_delta_progress(d_list, pp):
     d_list.sort(key=lambda x: x[1])
@@ -120,40 +154,41 @@ def main():
     if (len(sys.argv) > 3):
         alg = int(sys.argv[1])
         log_file = open(sys.argv[2], 'r')
-        new_file = open(sys.argv[3], 'w')
-        pp = PdfPages('plots_' + sys.argv[3]) 
+        summary_file = open(sys.argv[3], 'w')
+        delta_file = open('deltas_' + sys.argv[3], 'w')
+        timestep_file = open('timestep_' + sys.argv[3], 'w')
+        arm_file = open('arms_' + sys.argv[3], 'w')
+        # pp = PdfPages('plots_' + sys.argv[3]) 
     else:
         print("log file, and new file arguments are required, exiting...")
         sys.exit(0)
 
     # get how delta progresses over time (number of pulls)
-    delta_list = list()
-    parse_deltas(log_file, delta_list)
+    delta_list = parse_deltas(log_file)
+    write_deltas_to_file(delta_file, timestep_file, delta_list)
     
     # deltas (regrets) of the best arms
-    best_delta_list = list()
-    threshold = 0.01
-    parse_best_arm_deltas(log_file, best_delta_list)
+    best_delta_list = parse_best_arm_deltas(log_file)
+    write_summary_stats_to_file(summary_file, best_delta_list)
 
     # arm distributions (configmus)
     arm_dist = parse_arm_distribution(log_file)
-
-    # summary statistics
-    print("Average Best Arm Delta: %f" %(average_delta(best_delta_list)))
-    print("Success Rate with threshold: %f, is: %f" 
-            %(threshold, success_rate(best_delta_list, threshold)))
+    write_arms_to_file(arm_file, arm_dist)
     
     # plot the graphs and save them to pdfs
-    plot_arm_distribution(arm_dist, pp)
-    if alg == 0:
-        plot_egreedy_delta_progress(delta_list, pp)
-    else:
-        plot_seqhav_delta_progress(delta_list, pp)
+    # plot_arm_distribution(arm_dist, pp)
+    # if alg == 0:
+    #     plot_egreedy_delta_progress(delta_list, pp)
+    # else:
+    #     plot_seqhav_delta_progress(delta_list, pp)
 
     # close 
     log_file.close()
-    new_file.close()    
-    pp.close()
+    summary_file.close()    
+    delta_file.close()
+    timestep_file.close()
+    arm_file.close()
+    # pp.close()
 
 if __name__=="__main__":
     main()
