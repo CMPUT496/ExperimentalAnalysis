ac0386ec3bdabb2795a8900741dda530e9c08552
mfritze <mfritze@ualberta.ca>
2014-11-27 11:52:56 -0700

    More data, parsed, summarized and just plain awesome sauce

--- Simulator/Logs/format_lilucb.py
+++ Simulator/Logs/format_lilucb.py
@@ -1,18 +1,18 @@
 def reformat_logs(logFile, newLog):
-    delta_val = -1
+    #used for delta summary
+    delta_val = 0
     count = 0
     for line in logFile:
-        if(line[0:2] != 'IT'):
-			if(line[0:5] == "--- E"):
-				newLog.write("\n")
-				#print( count)
-			continue
-        components = line.split(" ")
-        delta_val = components[-1]
-        newLog.write("%s" %(delta_val))
+        if(line[0:5] == "--- E"):
+            newLog.write("\n")
+        if(line[0:2] == 'IT'):
+            components = line.split(" ")
+            delta_val = components[-1]
+            newLog.write("%s" %(delta_val))
         count += 1
 
 def averageResults(logFile, outFile, length, step):
+    #used for delta summary, arm summary
 	averages = [0 for i in range(length)]
 	last_val = 0
 	lc = 0
@@ -32,6 +32,7 @@ def averageResults(logFile, outFile, length, step):
 			outFile.write("%f\n" %(averages[i]/50))
 
 def armDistribution(log_file, out_file):
+    #used for regret summary
     for line in log_file:
         if(line[0] == 'A'):
             temp = line.split(' ')
@@ -41,15 +42,22 @@ def armDistribution(log_file, out_file):
     out_file.close()
 
 
-base = open("lilucb_50runs_underdist_Nov26.out", "r")
-log_file = open("regrets_summary_lilucb_underdist_nov27.data", "w")
-#arm_out_file = open("arms_summary_lilucb_conf2_Nov25", "w")
-#outfile = open("lilucb_50runs_base_largedist_averages_nov22", "w")
-#reformat_logs(base, log_file)
-#averageResults(log_file, outfile, 1001, 1)
+#base = open("lilucb_50runs_conf2_Nov20.out", "r")
+#log_file = open("regrets_summary_lilucb_overdist_nov27.data", "r")
+#arm_out_file = open("arms_summary_lilucb_largedist_Nov27.data", "w")
+#summary_file = open("pull_summary_lilucb_config2_nov27.data", "w")
+
+#reformat_logs(base, summary_file)
 #armDistribution(base, log_file)
-#log_file = open("condensed_results", "r")
-#averageResults(log_file, arm_out_file,720, 6)
-armDistribution(base, log_file)
-log_file.close()
-base.close()
+#summary_file.close()
+
+#Delta average
+summary_file = open("pull_summary_lilucb_largedist_nov27.data", "r")
+delta_out_file = open("deltas_summary_lilucb_largedist_nov27.data", "w")
+averageResults(summary_file, delta_out_file,1001, 1)
+
+#base.close()
+#log_file.close()
+#arm_out_file.close()
+summary_file.close()
+delta_out_file.close()
