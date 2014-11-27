42498674db959f7e39a534ed5f52b03b34e78121
mfritze <mfritze@ualberta.ca>
2014-11-27 11:21:29 -0700

    Added lilucb arm-regret summaries

--- Simulator/Logs/format_lilucb.py
+++ Simulator/Logs/format_lilucb.py
@@ -31,24 +31,25 @@ def averageResults(logFile, outFile, length, step):
 		if(i%step == 0):
 			outFile.write("%f\n" %(averages[i]/50))
 
-def armDistribution(logFile, tempFile):
-    for line in logFile:
+def armDistribution(log_file, out_file):
+    for line in log_file:
         if(line[0] == 'A'):
             temp = line.split(' ')
-            tempFile.write(temp[-1])
+            out_file.write(temp[-1])
         if(line[0:5] == "--- E"):
-                tempFile.write("\n")
-    tempFile.close()
+                out_file.write("\n")
+    out_file.close()
 
 
-base = open("lilucb_50runs_baseconf_Nov20.out", "r")
-temp = open("deltas_NON_summary_lilucb_base_nov25.data", "w")
-#outfile = open("lilucb_50runs_base_largedist_averages_nov22", "w")
+base = open("lilucb_50runs_underdist_Nov26.out", "r")
+log_file = open("regrets_summary_lilucb_underdist_nov27.data", "w")
 #arm_out_file = open("arms_summary_lilucb_conf2_Nov25", "w")
-reformat_logs(base, temp)
-#averageResults(temp, outfile, 1001, 1)
-#armDistribution(base, temp)
-#temp = open("condensed_results", "r")
-#averageResults(temp, arm_out_file,720, 6)
-temp.close()
+#outfile = open("lilucb_50runs_base_largedist_averages_nov22", "w")
+#reformat_logs(base, log_file)
+#averageResults(log_file, outfile, 1001, 1)
+#armDistribution(base, log_file)
+#log_file = open("condensed_results", "r")
+#averageResults(log_file, arm_out_file,720, 6)
+armDistribution(base, log_file)
+log_file.close()
 base.close()
